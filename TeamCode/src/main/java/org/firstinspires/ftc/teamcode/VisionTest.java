88652628acd3ba409b6f757286cf5088a692ccff
sam <sam@hortonvillerobotics.com>
2018-01-12 19:58:04 -0600

    here ya go ben

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTest.java
@@ -0,0 +1,102 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.content.Context;
+import android.os.Looper;
+import android.util.Log;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.vuforia.PIXEL_FORMAT;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+
+import static org.firstinspires.ftc.robotcontroller.external.samples.ConceptVuMarkIdentification.TAG;
+
+/**
+ * Created by sam on 1/8/2018.
+ */
+@TeleOp(name = "Vision")
+public class VisionTest extends StateMachine_v6 {
+
+    StateMachine_v6 vision1 = new StateMachine_v6();
+    StateMachine_v6 vision2 = new StateMachine_v6();
+
+    @Override
+    public void init() {
+        super.init();
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        parameters.vuforiaLicenseKey = "AXlr9dj/////AAAAGRujCnGOL0aIpjtd4y5IK2sFwI4jstOeOlytTrPr3jzeQQ9tGEHgLmuGdxzuxGNY5641pyXeeyJHccL+I4QCZq8Sodm5DAUBsAQQ9ox1EY3+KNfZISN06k1IqDf7YaRXhE02j+7aE4Apnm3Hvn9V5CDKSTgOq73eJId9uzHkuNaIx+UDV4fRS1HK5L6dSGmIw3+RW7uaFdBF0E2bvWzzpZv51KFzw5oy/9qFB9r6ke5B5Gd2zw9JjafwudFSpLVCiNzIreoTaIFYVMmGMuIFIT4C6oC13EcvbNl5CFpl+irLqhSI//nlL2L2DKxKtW5UTQqNBlOSBdTxWR/kSN12edlwOu0kFgzhKBFapn3KHC0V";
+
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        Vuforia.setFrameFormat(PIXEL_FORMAT.RGB565, true);
+
+        VuforiaTrackables relicTrackables = vuforia.loadTrackablesFromAsset("RelicVuMark");
+        relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+
+        telemetry.addData(">", "Press Play to start");
+        telemetry.update();
+
+        relicTrackables.activate();
+
+        vuforia.setFrameQueueCapacity(1);
+
+
+
+
+        if (Looper.myLooper() == null) Looper.prepare();
+
+        Context c = hardwareMap.appContext;
+
+        BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(c) {
+            @Override
+            public void onManagerConnected(int status) {
+                switch (status) {
+                    case LoaderCallbackInterface.SUCCESS: {
+                        Log.i(TAG, "OpenCV loaded successfully");
+                    }
+                    break;
+                    default: {
+                        super.onManagerConnected(status);
+                    }
+                    break;
+                }
+            }
+        };
+
+        Log.i(TAG, "Trying to load OpenCV library");
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, c, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    public void loop() {
+        vision1.initializeMachine();
+        vision2.initializeMachine();
+
+        vision1.ProcessRelic();
+        vision2.ProcessJewels(vuforiaFrameToMat());
+
+        if(vision1.next_state_to_execute()){
+            vision1.state_in_progress = 1;
+        }
+        if(vision2.next_state_to_execute()){
+            vision2.state_in_progress = 1;
+        }
+    }
+}
